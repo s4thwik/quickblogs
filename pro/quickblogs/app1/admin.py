de02f20d420c0c926cba4380d428034bf5980677
s4thwik <sathwikramesh24@gmail.com>
2024-10-03 03:54:12 +0530

    Delete pro directory

--- pro/quickblogs/app1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-
-from.models import *
-admin.site.register(user)
-admin.site.register(posts)
-# Register your models here.
-
-# Register your models here.
