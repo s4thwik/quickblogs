dda513ba6a87d2796e00b44472efd28640873eba
s4thwik <sathwikramesh24@gmail.com>
2024-10-03 03:57:40 +0530

    Code commit

---
+++ quickblogs/app1/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib import admin
+
+from.models import *
+admin.site.register(user)
+admin.site.register(posts)
+# Register your models here.
+
+# Register your models here.
